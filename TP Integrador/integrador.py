b284b89d240a7d0b7b64edf06ca7f4f823806476
JesuRami <jarbompart09@gmail.com>
2025-09-17 15:45:56 -0300

    Añadido algo; no estoy seguro de qué.

---
+++ TP Integrador/integrador.py
@@ -0,0 +1,10 @@
+golosinas = [[0, 1, 2, 3],
+             [4, 5, 6, 7],
+             [8, 9, 10, 11],
+             [12, 13, 14, 15],
+             [16, 17, 18, 19]]
+
+for i in range(0,len(golosinas)):
+    for j in range(0,len(golosinas[0])):
+        print(golosinas[i][j], end=' ')
+    print()
---
+++ TP Integrador/test.py
@@ -0,0 +1,6 @@
+lista = [[0,1,2,3,4,5], [6,7,8,9,10,11]]
+
+for i in range(0,len(lista)):
+    for j in range(0,len(lista[0])):
+        print(lista[i][j], end=' ')
+    print()
